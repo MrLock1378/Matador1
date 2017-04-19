9f953f5268c6b272f45a2d3a1083568a44025d5c
MrLock1378 <rezafaghihi1379@gmail.com>
2017-04-19 11:12:38 +0430

    Update Time.lua

--- plugins/Time.lua
+++ plugins/Time.lua
@@ -1,17 +1,15 @@
---Begin Time.lua By @MahDiRoO
-function run(msg, matches)
-local url , res = http.request('http://api.gpmod.ir/time/')
-if res ~= 200 then return "No connection" end
+do
+
+localfunction Time(msg, matches)
+if matches[1]:lower() == 'time' or matches[1]:lower() == 'Time' or matches[1]:lower() == 'ساعت' then
+local url , res = http.request('http://irapi.ir/time/')
+if res ~= 200 then return "No connection :)
+      " end
 local jdat = json:decode(url)
-local text = '*Ir Time:* _'..jdat.FAtime..'_\n*Ir Data:* _'..jdat.FAdate..'_\n------------\n*En Time:* _'..jdat.ENtime..'_\n *En Data:* _'..jdat.ENdate.. '_\n'
+local text = '🗓*Ir Time:* _'..jdat.FAtime..'_\n*🗓Ir Data:* _'..jdat.FAdate..'_\n------------\n*💎En Time:* _'..jdat.ENtime..'_\n *💎En Data:* _'..jdat.ENdate.. '_\n'
   tdcli.sendMessage(msg.chat_id_, 0, 1, text, 1, 'md')
-end
+	end
 return {
-  patterns = {
-  "^[/!]([Tt][iI][Mm][Ee])$",
-  "^([Tt][iI][Mm][Ee])$",
-  "^(ساعت)$"
-  }, 
-run = run 
+  patterns = {"^[/!]([Tt][iI][Mm][Ee])$","^(ساعت)$"}, 
+run = Time
 }
---End Time.lua--
