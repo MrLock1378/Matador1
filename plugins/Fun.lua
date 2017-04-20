fb3d18daa07a49c7d1f94d4a0dc9abdedaa687ad
MrLock1378 <rezafaghihi1379@gmail.com>
2017-04-20 18:35:29 +0430

    Update Fun.lua

--- plugins/Fun.lua
+++ plugins/Fun.lua
@@ -209,10 +209,10 @@ function run(msg, matches)
 	if matches[1]:lower() == 'time' or matches[1]:lower() == "ساعت" then
 		local url , res = http.request('http://irapi.ir/time')
 		if res ~= 200 then
-			return "No connection :)"
+			return "No ❌connection❌ :)"
 		end
 		local colors = {'black','blue','green','yellow','magenta','Orange','DarkOrange','red'}
-		local fonts = {'mathbf','mathit','mathfrak','mathrm'}
+		local fonts = {'mathit','mathrm'}
 		local jdat = json:decode(url)
 		local url = 'http://latex.codecogs.com/png.download?'..'\\dpi{600}%20\\huge%20\\'..fonts[math.random(#fonts)]..'{{\\color{'..colors[math.random(#colors)]..'}'..jdat.ENtime..'}}'
 		local file = download_to_file(url,'time.webp')
